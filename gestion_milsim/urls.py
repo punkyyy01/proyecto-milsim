91e06c85efa2e91537aa1d62cb08f184b14bf76d
Punky <aikodiaz45@gmail.com>
2026-02-15 11:23:06 -0300

    Redirect root to /orbat/ to avoid Not Found on root

--- gestion_milsim/urls.py
+++ gestion_milsim/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 from orbat.views import orbat_visual
 from orbat.audit_views import audit_log_list, audit_log_detail
 
 urlpatterns = [
+    # Redirige la raíz del sitio al ORBAT visual
+    path('', lambda request: redirect('orbat_visual')),
     path('admin/auditoria/', audit_log_list, name='audit_log_list'),
     path('admin/auditoria/<int:entry_id>/', audit_log_detail, name='audit_log_detail'),
     path('admin/', admin.site.urls),
